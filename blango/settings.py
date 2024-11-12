637190d1742ba0b72f9000b6736d647ce4ec404f
Miguel Pineda <miguelva703@gmail.com>
2024-11-12 17:47:04 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -90,6 +90,7 @@ class Dev(Configuration):
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
       'rest_framework',
+      'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -138,6 +139,15 @@ class Dev(Configuration):
 
     DATABASES = values.DatabaseURLValue(f"sqlite:///{BASE_DIR}/db.sqlite3")
 
+    
+
+    REST_FRAMEWORK = {
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ]
+    }
 
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,15 +1,21 @@
 from rest_framework import generics
-
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from blog.api.serializers import PostSerializer
 from blog.models import Post
-
+from rest_framework.permissions import IsAuthenticated
 
 class PostList(generics.ListCreateAPIView):
+
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
