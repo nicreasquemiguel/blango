e6c6cf4a0151355239a988dbefac99c1083e36cf
Miguel Pineda <miguelva703@gmail.com>
2024-11-12 19:01:58 +0000

    Finish related fields

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from blog.api.serializers import PostSerializer
+from blango_auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
 from rest_framework.permissions import IsAuthenticated
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
@@ -21,3 +22,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
